809d6ba6b18a7b105808cc7a0b7fcf6858924819
AbeilleTueuse <baptiste.ferrato1@orange.fr>
2024-01-03 12:46:43 +0100

    token

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from nextcord.ext.commands import Bot
 
 
 if __name__ == "__main__":
-    # TOKEN = input("Enter the discord bot token: ")
-    TOKEN = "MTE5MjA2MjU0NDU2Nzg2NTQyNQ.Gc0oA6.zreJ4cN-lPLdyOJCHpVTQ5GqoUyhpJHpRqtoP0"
+    TOKEN = input("Enter the discord bot token: ")
     
     intents = Intents.default()
     intents.message_content = True
